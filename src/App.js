ad783723ff60b7f0f4a8f51ba5a4e7e023b80d52
alexmsc96 <alexandrumuscina@gmail.com>
2020-05-08 21:56:45 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -33,11 +33,7 @@ class App extends React.Component {
           <img className={styles.logo} src={Covid19Logo} alt="Covid-19" />
           <Nav />
           <Switch>
-            <Route
-              exact
-              path={process.env.PUBLIC_URL + "/"}
-              component={this.homePage}
-            />
+            <Route exact path="/covid19" component={this.homePage} />
             <Route path="/news" component={News} />
             <Route path="/charts" component={Chart} />
           </Switch>
