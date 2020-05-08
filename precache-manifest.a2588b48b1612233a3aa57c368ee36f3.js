2ddabf7f8eee2904117db7f708387fdc6bbc5170
alexmsc96 <alexandrumuscina@gmail.com>
2020-05-08 21:22:39 +0300

    Updates

---
+++ precache-manifest.a2588b48b1612233a3aa57c368ee36f3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "488bd7bd4f5cd0824686e6bb6f642ada",
+    "url": "/covid19/index.html"
+  },
+  {
+    "revision": "40a4a55ce853b9662d17",
+    "url": "/covid19/static/css/main.a832181c.chunk.css"
+  },
+  {
+    "revision": "9788a99eeca5c202fac6",
+    "url": "/covid19/static/js/2.98060205.chunk.js"
+  },
+  {
+    "revision": "2cc63ca05e8441c44ab7a24a4afa7ce3",
+    "url": "/covid19/static/js/2.98060205.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40a4a55ce853b9662d17",
+    "url": "/covid19/static/js/main.da878ff7.chunk.js"
+  },
+  {
+    "revision": "13050c66b8110e64ca78",
+    "url": "/covid19/static/js/runtime-main.61b1d6d4.js"
+  },
+  {
+    "revision": "e0c652f83b8b98ca7ad1b6831f63d2d0",
+    "url": "/covid19/static/media/Covid19.e0c652f8.svg"
+  }
+]);
